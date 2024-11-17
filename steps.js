1a2f57e02e7cbd48f4fca77fdc78961ec181a715
Jay Khandagale <87541442+Jay9621@users.noreply.github.com>
2024-11-17 10:25:57 +0530

    Create steps.js

---
+++ steps.js
@@ -0,0 +1,8 @@
+// Simple code to display assembly step labels based on events
+var labelId = "label-2";
+$scope.$on('newStep', function(evt, arg) {
+  $scope.setWidgetProp(labelId, "text", arg);
+  var labelText = $scope.view.wdg[labelId].text;
+  var instructText = labelText.substr(labelText);
+  $scope.setWidgetProp(labelId, "text", instructText);
+});
